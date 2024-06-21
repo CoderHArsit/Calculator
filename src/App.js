d654ef935eb259da37eadefaddf82202b2861cd2
CoderHArsit <hs8449865663@gmail.com>
2024-06-21 14:53:01 +0530

    Added more functionalities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Calculator from "./components/Calculator";
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
 
 const App = () => {
+  const [toggle,setToggle]=useState('dark');
+  const [isDarkMode, setDarkMode] = React.useState(true);
+  const handletog=(checked)=>{
+    
+    setToggle(toggle==='light'?'dark':'light');
+    setDarkMode(checked);
+
+  }
   return (
-    <div className="App">
-      <Calculator />
+    <div className={`App ${toggle}`}>
+      <div style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
+      <h2>Confetti Calculator</h2> 
+       <p>
+       <DarkModeSwitch
+       style={{marginBlock:"20px",marginLeft:"20px"}}
+      checked={isDarkMode}
+      onChange={handletog}
+      size={30}
+    />
+       </p>
+      </div>
+      
+      <Calculator toggle={toggle} />
       
     </div>
   );
